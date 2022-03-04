37558bd4616bc9fcd9c9cbde127f1d943dba22c4
Cameron O'Leary <cameron@Camerons-PC.localdomain>
2022-03-03 22:27:30 -0500

    Refactored message passing test

--- server_synchronous.c
+++ server_synchronous.c
@@ -3,28 +3,35 @@
 #include <sys/neutrino.h>
 #include <process.h>
 #include <errno.h>
+#include <unistd.h>
 
 #define MAX_STRING_LEN    256
 
-int main(void) {
-
+int main(void)
+{
 	//Declare variables
 	int chid, rcvid;
 	pid_t pid;
 	char rmsg[MAX_STRING_LEN];
+
 	//create a channel
 	chid = ChannelCreate(0);
+
 	if(chid == -1){
 		perror("ChannelCreate()");
 		exit(-1);
 	}
+
 	pid = getpid();
+
 	printf("chid = %d; pid = %d\n", chid, pid);
+
 	while(1){
 		rcvid = MsgReceive(chid, &rmsg, sizeof(rmsg), NULL);
+		printf("Message received: %s\n", rmsg);
 		char* replymsg = "1";
 		MsgReply(rcvid, EOK, &replymsg, sizeof(replymsg));
 	}
 
-return 0;
+	return 0;
 }
