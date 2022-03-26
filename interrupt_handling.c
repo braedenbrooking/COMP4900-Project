e0ca24597f95dd362e5718d6e38b73fefb5d64ed
braedenbrooking <57050183+braedenbrooking@users.noreply.github.com>
2022-03-26 18:06:09 -0400

    Still W.I.P

--- interrupt_handling.c
+++ interrupt_handling.c
@@ -8,6 +8,7 @@
 #include <sys/syspage.h>
 #include <sys/procmgr.h>
 #include <unistd.h>
+//#include <sys/irqinfo.h>
 
 #define MAX_STRING_LEN		256
 #define ORDER_OF_MAGNITUDE	3
@@ -29,12 +30,14 @@ int main(int argc, char **argv)
 
 	child_pid = fork();
 
-	if(child_pid != 0){
+	if(child_pid == 0){
 		server(&t2);
 	}else{
 		client(&t1, child_pid);
 	}
-	while ((wpid = wait(&status)) >  0);
+	printf("And now we wait...\n");
+	//while ((wpid = wait(&status)) >  0);
+	waitpid(child_pid, &status, 0);
 
 	double TP = (t2.tv_sec - t1.tv_sec) + ((double) (t2.tv_nsec + t1.tv_nsec)) / BILLION;
 	double result = TP/NUMBER_OF_RUNS/2;
@@ -43,45 +46,55 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-const struct sigevent* handler(void *area) {
+const struct sigevent* handler(void *area, int n) {
 	if(++counter == NUMBER_OF_RUNS){
 		counter = 0;
 		return (&event);
 	}else{
+		printf("Interrupted\n");
 		return(NULL);
 	}
 }
 
 void server(struct timespec* t2){
-	printf("Server Ready\n");
-	event.sigev_notify = SIGEV_INTR;
+
+	SIGEV_INTR_INIT(&event);
+	//event.sigev_notify = SIGEV_INTR;
+	ThreadCtl(_NTO_TCTL_IO,0);
 
 	procmgr_ability(0,PROCMGR_ADN_ROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_INTERRUPT,PROCMGR_AID_EOL);
 
-	int id = InterruptAttach( SYSPAGE_ENTRY(qtime)->intr, &handler, NULL, 0, 0 );
-
-	InterruptWait( 0, NULL );
-	printf("Successfully Interrupt\n");
+	//int id = InterruptAttach( SYSPAGE_ENTRY(qtime)->intr, &handler, NULL, 0, 0 );
+	int id = InterruptAttachEvent(1, &event, _NTO_INTR_FLAGS_END|_NTO_INTR_FLAGS_PROCESS);
+	//int id = qnx_hint_attach(0, &handler, FP_SEG(&counter));
+	printf("Server Ready\n");
+	//for(int i =0; i< NUMBER_OF_RUNS; i++){
+		printf("waiting for interrupt\n");
+		InterruptWait( 0, NULL );
+		printf("Successfully Interrupt\n");
+	//}
 
 	if(clock_gettime(CLOCK_REALTIME, t2) == -1) {
 		perror("clock gettime");
 		exit(-1);
 	}
 	InterruptDetach(id);
-
+	printf("Detached\n");
 	exit(0);
 }
 
 void client(struct timespec* t1, pid_t pid){
 	sleep(1); // Shitty way of making sure the server is ready might need changing later but for now this is fine lmao
+	printf("Client Ready\n");
 	if(clock_gettime(CLOCK_REALTIME, t1) == -1) {
 		perror("clock gettime");
 		exit(-1);
 	}
 
-	for(int i = 0; i < NUMBER_OF_RUNS; i++) {
+	for(int i = 0; i < NUMBER_OF_RUNS; i++)
 		kill(pid, SIGINT);
-	}
+
+	printf("client done\n");
 
 	return;
 }
