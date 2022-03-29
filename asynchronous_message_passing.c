e422d21fd0ca6b99239664583ae432ce9fbcf14c
Cameron O'Leary <cameronmoleary@gmail.com>
2022-03-28 22:11:11 -0400

    Fixed async. message passing test.

--- asynchronous_message_passing.c
+++ asynchronous_message_passing.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/neutrino.h>
+// Asynchronous message passing test.
+// Calculates the duration of a pipe message transfer (TP).
+
+#include <math.h>
 #include <time.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <math.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/neutrino.h>
 
-
-#define MAX_STRING_LEN		256
-#define ORDER_OF_MAGNITUDE	3
-#define NUMBER_OF_RUNS		1000000
-#define BILLION				1000000000
+#define ORDER_OF_MAGNITUDE	0 // Tests: 0, 1, 2, 3, 4.
+#define NUMBER_OF_RUNS		10E6
+#define BILLION				1E9
 
 void server(int,int);
 void client(int,int,int);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	int pid;
 	int first_pipefd[2];
 	int second_pipefd[2];
 
-	if(pipe(first_pipefd) || pipe(second_pipefd)){
-		printf("Error opening pipe\n");
+	if (pipe(first_pipefd) || pipe(second_pipefd)) {
+		perror("pipe");
 		exit(-1);
 	}
 
+	fcntl(first_pipefd[0], F_SETFL, O_NONBLOCK);
+	fcntl(first_pipefd[1], F_SETFL, O_NONBLOCK);
+	fcntl(second_pipefd[0], F_SETFL, O_NONBLOCK);
+	fcntl(second_pipefd[1], F_SETFL, O_NONBLOCK);
+
 	pid = fork();
 
-	if(pid == 0){
+	if (pid == 0) {
 		server(first_pipefd[0], second_pipefd[1]);
-	}else{
+	} else {
 		client(first_pipefd[1], second_pipefd[0], pid);
 	}
 
@@ -44,50 +49,48 @@ int main(int argc, char **argv)
 	close(second_pipefd[0]);
 	close(second_pipefd[1]);
 
-
-
 	return 0;
 }
 
-void server(int read_pipe, int write_pipe){
-	printf("Server Ready\n");
-	int len = (int) pow(10, ORDER_OF_MAGNITUDE); // E.g., 1000 bytes.
+void server(int read_pipe, int write_pipe) {
+	printf("Server ready (pid = %d).\n", getpid());
+	int len = (int)pow(10, ORDER_OF_MAGNITUDE);
 	char *str = malloc(len + 1);
 
-	while(1){
+	while(1) {
 		read(read_pipe, str, len);
 		write(write_pipe, str, len);
 	}
 }
 
-void client(int write_pipe, int read_pipe, int server_pid){
-	printf("Client Ready\n");
-	int len = (int) pow(10, ORDER_OF_MAGNITUDE); // E.g., 1000 bytes.
+void client(int write_pipe, int read_pipe, int server_pid) {
+	printf("Client ready (server pid = %d).\n", server_pid);
+
+	struct timespec t0, t1;
+	int len = (int)pow(10, ORDER_OF_MAGNITUDE);
 	char *str = malloc(len + 1);
+
 	memset(str, '*', len);
 	str[len] = '\0';
 
-	struct timespec t1,t2;
-	if(clock_gettime(CLOCK_REALTIME, &t1) == -1) {
-		perror("clock gettime");
+	if (clock_gettime(CLOCK_REALTIME, &t0) == -1) {
+		perror("clock_gettime");
 		exit(-1);
 	}
 
-	for(int i = 0; i < NUMBER_OF_RUNS; i++) {
+	for (int i = 0; i < NUMBER_OF_RUNS; i++) {
 		write(write_pipe, str, len);
 		read(read_pipe, str, len);
 	}
 
-	if(clock_gettime(CLOCK_REALTIME, &t2) == -1) {
-		perror("clock gettime");
+	if (clock_gettime(CLOCK_REALTIME, &t1) == -1) {
+		perror("clock_gettime");
 		exit(-1);
 	}
 
+	double t = (double)((t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / BILLION);
+	double TP = (t / NUMBER_OF_RUNS / 2) * 1000000;
 
-	double TP = (t2.tv_sec - t1.tv_sec) + ((double) (t2.tv_nsec + t1.tv_nsec)) / BILLION;
-	double result = TP/NUMBER_OF_RUNS/2;
-
-	printf("%d B @ %lf s\n", len, result);
-
+	printf("%d B @ %lf us\n", len, TP);
 	kill(server_pid, SIGKILL);
 }
