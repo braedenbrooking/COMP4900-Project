cc5199e32fcf61966a9de27995daf246c584bd77
Cameron O'Leary <cameronmoleary@gmail.com>
2022-03-30 19:36:48 -0400

    Added QNX interrupt template. Needs to be measured.

--- interrupt_handling.c
+++ interrupt_handling.c
@@ -1,100 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/neutrino.h>
-#include <time.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/syspage.h>
-#include <sys/procmgr.h>
-#include <unistd.h>
-//#include <sys/irqinfo.h>
+// Interrupt handling.
+// https://www.qnx.com/developers/docs/7.1/index.html#com.qnx.doc.neutrino.sys_arch/topic/kernel_Interrupt_calls.html
 
-#define MAX_STRING_LEN		256
-#define ORDER_OF_MAGNITUDE	3
-#define NUMBER_OF_RUNS		1000000
-#define BILLION				1000000000
+#include <stdio.h>
+#include <sys/procmgr.h>
+#include <sys/syspage.h>
+#include <sys/neutrino.h>
+
+#define COUNT 10
 
 struct sigevent event;
-volatile unsigned long int counter = 0;
+volatile unsigned counter;
 
-void server(struct timespec*);
-void client(struct timespec*, pid_t);
+const struct sigevent *handler(void *area, int id) {
+    // Wake up the thread every 100th interrupt
+    if (++counter == 100) {
+        counter = 0;
+        return &event;
+	} else
+        return NULL;
+    }
 
-int main(int argc, char **argv)
-{
-	struct timespec t1,t2;
+int main() {
+	int i;
+	int id;
 
-	pid_t child_pid, wpid;
-	int status = 0;
+	// Initialize event structure
+	event.sigev_notify = SIGEV_INTR;
 
-	child_pid = fork();
+	// Enable the INTERRUPT ability
+	procmgr_ability(0, PROCMGR_ADN_ROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_INTERRUPT, PROCMGR_AID_EOL);
 
-	if(child_pid == 0){
-		server(&t2);
-	}else{
-		client(&t1, child_pid);
+	// Attach ISR vector
+	id = InterruptAttach(SYSPAGE_ENTRY(qtime)->intr, &handler, NULL, 0, 0);
+
+	for (i = 0; i < COUNT; ++i) {
+		// Wait for ISR to wake us up
+		InterruptWait(0, NULL);
+		printf("100 events\n");
 	}
-	printf("And now we wait...\n");
-	//while ((wpid = wait(&status)) >  0);
-	waitpid(child_pid, &status, 0);
 
-	double TP = (t2.tv_sec - t1.tv_sec) + ((double) (t2.tv_nsec + t1.tv_nsec)) / BILLION;
-	double result = TP/NUMBER_OF_RUNS/2;
-	printf(" %lf s\n", result);
+	// Disconnect the ISR handler
+	InterruptDetach(id);
 
 	return 0;
-}
-
-const struct sigevent* handler(void *area, int n) {
-	if(++counter == NUMBER_OF_RUNS){
-		counter = 0;
-		return (&event);
-	}else{
-		printf("Interrupted\n");
-		return(NULL);
-	}
-}
-
-void server(struct timespec* t2){
-
-	SIGEV_INTR_INIT(&event);
-	//event.sigev_notify = SIGEV_INTR;
-	ThreadCtl(_NTO_TCTL_IO,0);
-
-	procmgr_ability(0,PROCMGR_ADN_ROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_INTERRUPT,PROCMGR_AID_EOL);
-
-	//int id = InterruptAttach( SYSPAGE_ENTRY(qtime)->intr, &handler, NULL, 0, 0 );
-	int id = InterruptAttachEvent(1, &event, _NTO_INTR_FLAGS_END|_NTO_INTR_FLAGS_PROCESS);
-	//int id = qnx_hint_attach(0, &handler, FP_SEG(&counter));
-	printf("Server Ready\n");
-	//for(int i =0; i< NUMBER_OF_RUNS; i++){
-		printf("waiting for interrupt\n");
-		InterruptWait( 0, NULL );
-		printf("Successfully Interrupt\n");
-	//}
-
-	if(clock_gettime(CLOCK_REALTIME, t2) == -1) {
-		perror("clock gettime");
-		exit(-1);
-	}
-	InterruptDetach(id);
-	printf("Detached\n");
-	exit(0);
-}
-
-void client(struct timespec* t1, pid_t pid){
-	sleep(1); // Shitty way of making sure the server is ready might need changing later but for now this is fine lmao
-	printf("Client Ready\n");
-	if(clock_gettime(CLOCK_REALTIME, t1) == -1) {
-		perror("clock gettime");
-		exit(-1);
-	}
-
-	for(int i = 0; i < NUMBER_OF_RUNS; i++)
-		kill(pid, SIGINT);
-
-	printf("client done\n");
-
-	return;
 }
