2003dbadbab3b95dcde6688ae5c8a52547dbee65
Cameron O'Leary <cameronmoleary@gmail.com>
2022-03-30 13:08:41 -0400

    Fixed synchronization test.

--- synchronization.c
+++ synchronization.c
@@ -1,5 +1,6 @@
 // Task synchronization and event handling test.
 
+#include <math.h>
 #include <time.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,20 +8,30 @@
 #include <sys/neutrino.h>
 #include <sys/resource.h>
 
-#define COUNT	250 // Tests: 50, 100, 150, 200, 250.
+#define COUNT	15E3 // Tests: 3E3, 6E3, 9E3, 12E3, 15E3.
 #define BILLION	1E9
 
 int client_pid;
 
-void client(pid_t);
-void server(pid_t);
+void client(pid_t, sigset_t);
+void server(pid_t, sigset_t);
 void empty_handler();
 void handler(int);
 
 int main(int argc, char **argv) {
 	int pid, ppid;
+	sigset_t set, client_set, server_set;
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigaddset(&set, SIGUSR2);
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	sigemptyset(&client_set);
+	sigemptyset(&server_set);
 
 	signal(SIGUSR1, empty_handler);
+	signal(SIGUSR2, empty_handler);
 
 	pid = fork();
 	ppid = getppid();
@@ -32,29 +43,29 @@ int main(int argc, char **argv) {
 
 	if (pid == 0) {
 		// Child.
-		// Lower the priority of the server for signal handler level test.
-		// Comment this out if running the task level test.
-		// setpriority(PRIO_PROCESS, ppid, 19);
-		client(ppid);
+		client(ppid, client_set);
 	} else {
 		// Parent.
 		client_pid = pid;
-		server(client_pid);
+		server(client_pid, server_set);
 	}
 
 	return 0;
 }
 
-void client(pid_t server_pid) {
-	printf("Client ready (server_pid = %d).\n", server_pid);
+void empty_handler() {}
 
-	sigset_t set;
+void handler(int signum) {
+	signal(SIGUSR2, SIG_DFL);
+	kill(client_pid, SIGUSR2);
+}
+
+void client(pid_t server_pid, sigset_t set) {
 	struct timespec t0, t1;
+	double delta, t, TH, TT;
 
-	sigemptyset(&set);
-
-	// Comment this out if running the task level test.
-	// signal(SIGUSR2, empty_handler);
+	// Begin task level test.
+	printf("Measuring task level test, please wait...\n\n");
 
 	if (clock_gettime(CLOCK_REALTIME, &t0) == -1) {
 		perror("clock_gettime");
@@ -62,18 +73,7 @@ void client(pid_t server_pid) {
 	}
 
 	for (int i = 0; i < COUNT; i++) {
-		sleep(1);
-
-		// Task level test.
 		kill(server_pid, SIGUSR1);
-		printf("Sent SIGUSR1 to server.\n");
-
-		// Signal handler level test.
-		// Comment this out if running the task level test.
-		// kill(server_pid, SIGUSR2);
-		// printf("Sent SIGUSR2 to server.\n");
-
-		printf("Client suspending...\n");
 		sigsuspend(&set);
 	}
 
@@ -82,53 +82,59 @@ void client(pid_t server_pid) {
 		exit(-1);
 	}
 
-	// time_slept is the total number of seconds slept (i.e.,
-	// client and server each sleep 1s for every run). We need
-	// to subtract this amount from the total time taken to
-	// get the actual amount of time teken by the signal passing.
-	int time_slept = COUNT * 2;
-	printf("t = %f\n", (double)((t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / BILLION));
-	double TD = ((double)((t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / BILLION)) - time_slept;
-	double TT = TD / COUNT / 2;
+	delta = (double)((t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / BILLION);
+	TT = (delta / COUNT / 2) * 1000000;
 
 	// Task level results.
-	printf("%lf us\n\n", TT);
+	printf("Task Level Results:\n");
+	printf("t1    = %d.%ld s\n", (int)t1.tv_sec, t1.tv_nsec);
+	printf("t0    = %d.%ld s\n", (int)t0.tv_sec, t0.tv_nsec);
+	printf("delta = %lf s\n", delta);
+	printf("TT    = %lf us <----- RESULT\n\n", TT);
+
+	// Begin signal handler level test.
+	printf("Measuring signal handler level test, please wait...\n\n");
+
+	// Lower the priority of the server for signal handler level test.
+	setpriority(PRIO_PROCESS, server_pid, 19);
+
+	if (clock_gettime(CLOCK_REALTIME, &t0) == -1) {
+		perror("clock_gettime");
+		exit(-1);
+	}
+
+	for (int i = 0; i < COUNT; i++) {
+		kill(server_pid, SIGUSR2);
+		sigsuspend(&set);
+	}
+
+	if (clock_gettime(CLOCK_REALTIME, &t1) == -1) {
+		perror("clock_gettime");
+		exit(-1);
+	}
+
+	delta = (double)((t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / BILLION);
+	t = (delta / COUNT) * 1000000;
 
 	// Signal handler level results.
-	// Comment this out if running the task level test.
-	// double t = TD / COUNT;
-	// double TH = t - TT;
-	// printf("%lf us\n\n", TH);
+	printf("Signal Handler Level Results:\n");
+	printf("t1    = %d.%ld s\n", (int)t1.tv_sec, t1.tv_nsec);
+	printf("t0    = %d.%ld s\n", (int)t0.tv_sec, t0.tv_nsec);
+	printf("delta = %lf s\n", delta);
+	printf("t     = %lf us <----- RESULT\n\n", t);
+
+	TH = fabs(t - TT);
+
+	// Difference in results.
+	printf("Difference between TL and SHL results:\n");
+	printf("TH = %lf us\n", TH);
+
+	kill(server_pid, SIGKILL);
 }
 
-void server(pid_t client_pid) {
-	printf("Server ready (client_pid = %d).\n", client_pid);
-
-	sigset_t set;
-
-	sigemptyset(&set);
-
-	// Comment this out if running the task level test.
-	// signal(SIGUSR2, handler);
-
+void server(pid_t client_pid, sigset_t set) {
 	while (1) {
-		printf("Server suspending...\n");
 		sigsuspend(&set);
-
-		// Task level test.
-		// Comment this out if running the signal handler level test.
-		sleep(1);
 		kill(client_pid, SIGUSR1);
-		printf("Sent SIGUSR1 to client.\n");
 	}
-}
-
-void empty_handler() {}
-
-void handler(int signum) {
-	if (signum == SIGUSR1) return;
-
-	sleep(1);
-	kill(client_pid, SIGUSR2);
-	printf("Sent SIGUSR2 to client.\n");
 }
